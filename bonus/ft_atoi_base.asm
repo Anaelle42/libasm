33257996c2f3b37884d52d7a011f56bfdd4c9dda
ahenault <ahenault@student.42.fr>
2026-06-22 15:11:49 +0200

    refactor: improve ft_atoi_base

--- bonus/ft_atoi_base.asm
+++ bonus/ft_atoi_base.asm
@@ -5,109 +5,99 @@ section .text
 ft_atoi_base:
 	test rdi, rdi
 	je .invalid
-
 	test rsi, rsi
 	je .invalid
 
 	xor rcx, rcx
 
-.check_base_loop:
+.check_base:
 	mov al, [rsi + rcx]
 	test al, al
-	je .end_string
+	je .base_checked
 	
 	cmp al, '-'
 	je .invalid
-
 	cmp al, '+'
 	je .invalid
-
 	cmp al, ' '
 	je .invalid
-
-	mov rdx, rcx
-	inc rdx
-
 	cmp al, 9
-	jl .check_doublon
-
+	jl .check_duplicate
 	cmp al, 13
 	jle .invalid
 
-.check_doublon:
+.check_duplicate:
+	lea rdx, [rcx + 1]
+
+.dup_loop:
 	mov r8b, [rsi + rdx]
 	test r8b, r8b
-	je .next
+	je .next_char
 
 	cmp al, r8b
-	je .invalid ; doublon
+	je .invalid ; duplicate
 
 	inc rdx
-	jmp .check_doublon
+	jmp .dup_loop
 
-.next:
+.next_char:
 	inc rcx
-	jmp .check_base_loop
+	jmp .check_base
 
-.end_string:
+.base_checked:
 	cmp rcx, 2
 	jl .invalid
 
-	mov r8, rcx ; base_len
+	mov r8d, ecx ; base_len
 	mov r9d, 1 ; sign
-	xor rax, rax
+	xor eax, eax ; result
 
 .skip_spaces:
-	mov r11b, [rdi]
+	mov r10b, [rdi]
 
-	cmp r11b, ' '
-	je .space_next
+	cmp r10b, ' '
+	je .skip_one_space
+	cmp r10b, 9
+	jl .parse_sign
+	cmp r10b, 13
+	jle .skip_one_space
 
-	cmp r11b, 9
-	jl .check_sign
-
-	cmp r11b, 13
-	jle .space_next
-
-	jmp .check_sign
-
-.space_next:
-	inc rdi
-	jmp .skip_spaces
-
-.check_sign:
-	mov r11b, [rdi]
+.parse_sign:
+	mov r10b, [rdi]
 	
-	cmp r11b, '+'
-	je .sign_plus
-
-	cmp r11b, '-'
-	je .sign_moins
+	cmp r10b, '+'
+	je .plus
+	cmp r10b, '-'
+	je .minus
 
 	jmp .convert
 
-.sign_plus:
+.skip_one_space:
 	inc rdi
-	jmp .check_sign
+	jmp .skip_spaces
 
-.sign_moins:
+.plus:
+	inc rdi
+	jmp .parse_sign
+
+.minus:
 	neg r9d
 	inc rdi
-	jmp .check_sign
+	jmp .parse_sign
 
 .convert:
-	mov r11b, [rdi]
-	test r11b, r11b
-	je .end
+	mov r10b, [rdi]
+	test r10b, r10b
+	je .done
 
 	xor rdx, rdx
 
 .find_digit:
-	mov r10b, [rsi + rdx]
-	test r10b, r10b
-	je .end
+	mov r11b, [rsi + rdx]
+	test r11b, r11b
+	je .done
 
-	cmp r11b, r10b
+	cmp r10b, r11b
 	je .digit_found
 
 	inc rdx
@@ -115,8 +105,8 @@ ft_atoi_base:
 
 .digit_found:
 	; result = result * base_len + digit
-	imul rax, r8
-	add rax, rdx
+	imul eax, r8d
+	add eax, edx
 
 	inc rdi
 	jmp .convert
@@ -125,6 +115,6 @@ ft_atoi_base:
 	xor eax, eax
 	ret
 
-.end:
-	imul rax, r9
+.done:
+	imul eax, r9d
 	ret
